556baef6821558bef6ecabc4ef1cc60b9a529768
scrpti <pedroscarpati@uma.es>
2025-03-16 10:54:42 +0100

    practicaBanco finished - ready for submit

--- practicaBanco/bank.sol
+++ practicaBanco/bank.sol
@@ -3,16 +3,18 @@ pragma solidity >=0.4.16 <0.9.0;
 
 contract Bank {
 
-    uint256 vault = address(this).balance;
 
     mapping (address => uint256) balances;
     mapping (address => uint256) deudas;
+    mapping (address => uint256) timestampDeudas;
 
-    int256 coefCaja = 5;
-    int256 interesBanco = 10;
+    uint256 coefCaja = 5;
+    uint256 interesPrestamos = 1; //1% diario
+    uint256 plazoPrestamo = 7 days;
 
+    //Al desplegar el contrato, el dueño del contrato se define como owner para posteriormente obtener recompensas por los intereses de los prestamos
     constructor() payable {
-
+        owner = msg.sender;
     }
 
     //Cualquiera puede hacer un depósito de ETH, que queda asociado a su cuenta.
@@ -42,32 +44,62 @@ contract Bank {
     //El banco puede realizar préstamos con el dinero que tiene en depósito.
 
     function getLoan(uint256 _amount) external {
+        //El prestatario puede pedir una deuda como mucho el 50% de su balance, teniendo en cuenta que si la cantidad de su intento de prestamo mas su deuda 
+        //supera este 50% se le deniega la prestación
+
+        //Comprobacion de candidato para un prestamo
+        bool isEligibleForLoan = (deudas[msg.sender] + _amount) <= ((balances[msg.sender] * 50) / 100);
+        require(isEligibleForLoan, "Su cantidad de prestamo no puede superar el 50% de su balance");
+
+        //Comprobación de coefCaja válido
+        require(coefCaja < 100, "Coeficiente de caja debe ser menor a 100");
         address sender = msg.sender;
-        int256 vault = int256(address(this).balance) * ((100-coefCaja) / 100);
-            require(_amount <= vault, "No se pudo realizar el prestamo por falta de fondos");
+        uint256 vault = (address(this).balance * (100 - coefCaja)) / 100;
+
+        //Comprobación de fondos suficientes
+        require(_amount <= vault, "No se pudo realizar el prestamo por falta de fondos");
         deudas[sender] += _amount;
+        timestampDeudas[sender] = block.timestamp;
+
+        //Envío de dinero
         payable(sender).transfer(_amount);
     }
 
     //Ofrecer un método para consultar el saldo deudor de cualquier cuenta.
 
     function getLoanBalance(address _account) external view returns (uint256){
-        return deudas[_account];
+        return deudas[_account] + getInteres(_account);
     }
 
     //Los préstamos del banco puede tener asociado un tipo de interés conocido. Sería interesante que la
     //deuda fuera creciendo debido a los intereses conforme pasa el tiempo.
 
-    function getInteres(address _account) internal view returns (uint256){
-        uint256 interes = (deudas[_account] * interesPrestamos) / 100;
+    function getInteres(address _account) public view returns (uint256) {
+        uint256 timeElapsed = (block.timestamp - timestampDeudas[_account]) / 1 days; //Obtenemos el tiempo transcurrido en dias de la deuda
+        uint256 interes = 0;
+        //Idear algún mecanismo para que no se identifique a los prestatarios directamente como "deudores".
+        //Por ejemplo, se les puede dar un plazo para que devuelvan el préstamo, y si expira, ya se les incluye
+        //en la lista de "deudores"
+        if(timeElapsed >= plazoPrestamo && deudas[_account] > 1){
+            interes = (deudas[_account] * (timeElapsed * interesPrestamos)) / 100;
+        }
         return interes;
+
     }
 
     function payLoan() external payable{
         uint256 totalDebt = deudas[msg.sender] + getInteres(msg.sender);
+        uint256 profit = getInteres(msg.sender); 
         uint256 amount = msg.value;
         require(amount >= totalDebt, "El pago de la deuda no puede ser menor al total de la deuda");
 
+        deudas[msg.sender] = 0;
+        timestampDeudas[msg.sender] = 0;
+
+        //Sería más interesante aún que los beneficios generados por estos préstamos con interés fueran a
+        //parar al que creó el contrato
+
+        payable(owner).transfer(profit);
     }
 
 
